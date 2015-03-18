0c88b49f569037a3904509d8f466e156d0b9f138
Daniel Gampl <daniel_gampl@yahoo.co.uk>
2015-03-18 10:15:39 +0000

    Adding a readme file and removing the hardcoded path to the input file.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # addressBook
 A coding exercise for Gumtree
+
+
+
+
+How to build the solution:
+==========================
+
+To build the solution - simply execute the following command from the root of the project (where pom.xml is):
+
+mvn clean package
+
+
+
+
+How to run the solution:
+========================
+
+Navigate to "target" directory and execute the following command:
+
+java -jar addressBook-0.0.1-SNAPSHOT-jar-with-dependencies.jar ../inbox/AddressBook
--- src/main/java/com/neatcode/addressBook/main/Main.java
+++ src/main/java/com/neatcode/addressBook/main/Main.java
@@ -1,5 +1,6 @@
 package com.neatcode.addressBook.main;
 
+import java.io.File;
 import java.util.ArrayList;
 
 import com.neatcode.addressBook.exception.InvalidInputFileException;
@@ -9,10 +10,11 @@ import com.neatcode.addressBook.pojo.Person;
 public class Main {
 
 	public static void main(String[] args) {
+		checkParams(args);
 		InputFileParser parser = new InputFileParser();
 		ArrayList<Person> fileContent = null;
 		try {
-			fileContent = parser.parseInputFile("../inbox/AddressBook");
+			fileContent = parser.parseInputFile(args[0]);
 		} catch (InvalidInputFileException e) {
 			System.out.println(e.getMessage());
 			System.out.println("Invalid input file. Exiting...");
@@ -22,4 +24,16 @@ public class Main {
 
 	}
 
+	private static void checkParams(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Invalid number of arguments. Expected 1 argument.");
+			System.exit(-2);
+		}
+		
+		File inputFile = new File(args[0]);
+		if (!inputFile.canRead()) {
+			System.out.println("Can't read from the input file. Please check the first argument.");
+			System.exit(-3);
+		}
+	}
 }
